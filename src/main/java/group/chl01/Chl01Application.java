86d8a90cdf5f0d680c6b139f7a8414d91f82e798
kiririnshi <49988006+kiririnshi@users.noreply.github.com>
2020-12-13 18:47:07 +0100

    Add files via upload

---
+++ src/main/java/group/chl01/Chl01Application.java
@@ -0,0 +1,13 @@
+package group.chl01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chl01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chl01Application.class, args);
+	}
+
+}
---
+++ src/test/java/group/chl01/Chl01ApplicationTests.java
@@ -0,0 +1,13 @@
+package group.chl01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chl01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
